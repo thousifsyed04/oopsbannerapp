98f6da2be62b3a2c3c049072cae1a956df820d74
thousifsyed04 <thousifsyed28@gmail.com>
2026-02-23 17:55:00 +0530

    UC2: Add OOPSBannerApp2 implementation

---
+++ com.oopsbanner/OOPSBannerApp2.java
@@ -0,0 +1,14 @@
+public class OOPSBannerApp2 {
+   public OOPSBannerApp2() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("  *       *      **        **** ");
+      System.out.println(" * *     * *     *  *    **    ");
+      System.out.println("*   *   *   *    *   *  **    ");
+      System.out.println("*   *   *   *    * *      ***   ");
+      System.out.println("*   *   *   *    *          **  ");
+      System.out.println(" * *     * *     *         **  ");
+      System.out.println("  *       *      *     **** ");
+   }
+}
